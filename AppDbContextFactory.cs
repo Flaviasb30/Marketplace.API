6326885deb6e90052f32dee4d6941645c24e728d
Flávia De Souza Brida <flaviadesouzabrida@gmail.com>
2026-01-11 16:23:37 -0300

    Add files via upload

---
+++ AppDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Marketplace.API.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection"));
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Marketplace.API.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Categoria { get; set; }
+    public decimal Preco { get; set; }
+    public string ImagemUrl { get; set; }
+
+    public int VendedorId { get; set; }
+    public User Vendedor { get; set; }
+}
